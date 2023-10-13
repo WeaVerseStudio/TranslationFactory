f79fcd1fd1200fb7f1cfc8e29e78ff1b120ae38c
PrograMistV1 <programist.v.1.1@gmail.com>
2023-10-13 21:51:41 +0200

    Initial Commit

--- src/TranslationFactory.php
+++ src/TranslationFactory.php
@@ -4,8 +4,36 @@ declare(strict_types=1);
 
 namespace PrograMistV1\TranslationFactory;
 
+use pocketmine\lang\Language;
+use pocketmine\lang\Translatable;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 
 class TranslationFactory extends PluginBase{
 
+    private const DEFAULT_LANG = "en_us";
+
+    /**
+     * @var Language[]
+     */
+    private static array $languages = [];
+
+    protected function onEnable() : void{
+        $this->saveResource(self::DEFAULT_LANG.".ini");
+        $files = scandir($this->getDataFolder());
+        foreach($files as $file){
+            if(preg_match('/\.(ini)/', $file)){
+                $langCode = str_replace(".ini", "", $file);
+                self::$languages[$langCode] = new Language($langCode, $this->getDataFolder(), self::DEFAULT_LANG);
+            }
+        }
+    }
+
+    public static function translate(Player $player, Translatable $translatable) : string{
+        $playerLang = strtolower($player->getPlayerInfo()->getExtraData()["LanguageCode"]);
+        if(in_array($playerLang, array_keys(self::$languages))){
+            return self::$languages[$playerLang]->translate($translatable);
+        }
+        return self::$languages[self::DEFAULT_LANG]->translate($translatable);
+    }
 }
