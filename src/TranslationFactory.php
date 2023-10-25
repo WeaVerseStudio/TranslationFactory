09945686a4d29501b7b19a01d0ab6776d0e1b1db
PrograMistV1 <programist.v.1.1@gmail.com>
2023-10-25 12:46:11 +0200

    all vanilla languages

--- src/TranslationFactory.php
+++ src/TranslationFactory.php
@@ -8,10 +8,46 @@ use pocketmine\lang\Language;
 use pocketmine\lang\Translatable;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use Symfony\Component\Filesystem\Path;
 
 class TranslationFactory extends PluginBase{
 
     private const DEFAULT_LANG = "en_us";
+
+    /**
+     * @link https://wiki.bedrock.dev/concepts/text-and-translations.html#vanilla-languages
+     */
+    private const ALL_LANG_CODES = [
+        "id_id" => "Indonesian",
+        "da_dk" => "Danish",
+        "de_de" => "German",
+        "en_gb" => "English",
+        "en_us" => "English",
+        "es_es" => "Spanish",
+        "es_mx" => "Mexican Spanish",
+        "fr_ca" => "Canadian French",
+        "fr_fr" => "French",
+        "it_it" => "Italian",
+        "hu_hu" => "Hungarian",
+        "nl_nl" => "Dutch",
+        "nb_nb" => "Bokmål",
+        "pl_pl" => "Polish",
+        "pt_br" => "Brazilian Portuguese",
+        "pt_pt" => "Portuguese",
+        "sk_sk" => "Slovak",
+        "fi_fi" => "Finnish",
+        "sv_se" => "Swedish",
+        "tr_tr" => "Turkish",
+        "cs_cz" => "Czech",
+        "el_gr" => "Greek",
+        "bg_bg" => "Bulgarian",
+        "ru_ru" => "Russian",
+        "uk_ua" => "Ukrainian",
+        "ja_jp" => "Japanese",
+        "zh_cn" => "Chinese (Simplified)",
+        "zh_tw" => "Chinese (Traditional)",
+        "ko_kr" => "Korean"
+    ];
 
     /**
      * @var Language[]
@@ -19,7 +55,14 @@ class TranslationFactory extends PluginBase{
     private static array $languages = [];
 
     protected function onEnable() : void{
-        $this->saveResource(self::DEFAULT_LANG.".ini");
+        foreach(self::ALL_LANG_CODES as $lang_code => $language){
+            $path=Path::join($this->getDataFolder(), $lang_code.".ini");
+            if(!file_exists($path)){
+                $file = fopen($path, "w");
+                fwrite($file, "language.name=".$language);
+                fclose($file);
+            }
+        }
         $files = scandir($this->getDataFolder());
         foreach($files as $file){
             if(preg_match('/\.(ini)/', $file)){
